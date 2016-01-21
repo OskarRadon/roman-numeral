b432382fc38d4a192ca893477f14fefd03d49bcd
Afton Downey and Oskar Radon <aaike001@gmail.com>
2016-01-21 13:27:20 -0800

    add first test to split number

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+var romanNumerals = function(number) {
+  var digits = number.toString().split('');
+  return digits;
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("romanNumerals", function() {
+  it("will take a given number and split it into an array of digits", function(){
+    expect(romanNumerals(2362)).to.eql(["2","3","6","2"]);
+  });
+});
