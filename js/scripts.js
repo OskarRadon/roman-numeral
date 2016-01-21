acc01ecf8bd1d9803946cbebe3c7d9f5cf82fe89
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-21 13:49:54 -0800

    Add test for if number is longer than 4 digits

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 
 var romanNumerals = function(number) {
   var digits = number.toString().split('').map(parseFloat);
+
+  if (digits.length > 4) {
+    return true;
+  } else {
   return digits;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -4,5 +4,9 @@ describe("romanNumerals", function() {
     expect(romanNumerals(2362)).to.eql([2,3,6,2]);
   });
 
-  it("")
+  it("will throw error if number is longer than 4 digits", function(){
+    expect(romanNumerals(12345)).to.equal(true);
+  });
+  // it("will throw error if number in 1's place is >3")
+  // it("will convert number in 1's place to roman numeral")
 });
