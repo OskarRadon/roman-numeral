c5d9e4610902e1ded254adcc65972a0d2fbd0148
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-21 16:32:04 -0800

    Attempt to compute final result

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,59 @@
 var digits = [];
+var thousandsPlace = digits[digits.length - 4];
 
 var romanNumerals = function(number) {
   digits = number.toString().split('').map(parseFloat);
   if ((digits.length > 4) || (digits[0] > 3)){
     return true;
   } else {
-  return digits;
+  return thousandsPlace;//.concat(hundredsPlace, tensPlace, onesPlace);
   }
 };
-var onesPlace = digits[digits.length - 1];
 
+var onesPlace = digits[digits.length - 1];
 var ones = function(onesPlace) {
+
   var numbers = [1,2,3,4,5,6,7,8,9];
   var numerals = ["I","II","III","IV","V","VI","VII","VIII","IX"];
 
   var index = numbers.indexOf(onesPlace);
-
   onesPlace = numerals[index];
 
-
   return onesPlace;
+};
+
+var tensPlace = digits[digits.length - 2];
+var tens = function(tensPlace) {
+
+  var numbers = [1,2,3,4,5,6,7,8,9];
+  var numerals = ["X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+
+  var index = numbers.indexOf(tensPlace);
+  tensPlace = numerals[index];
+
+  return tensPlace;
+};
+
+var hundredsPlace = digits[digits.length - 3];
+var hundreds = function(hundredsPlace) {
+
+  var numbers = [1,2,3,4,5,6,7,8,9];
+  var numerals = ["C","CC","CCC","CD","D","DC","DCC","DCCC","CD"];
+
+  var index = numbers.indexOf(hundredsPlace);
+  hundredsPlace = numerals[index];
+
+  return hundredsPlace;
+};
 
 
+var thousands = function(thousandsPlace) {
 
+  var numbers = [1,2,3];
+  var numerals = ["M","MM","MMM"];
 
+  var index = numbers.indexOf(thousandsPlace);
+  thousandsPlace = numerals[index];
 
-  // if (onesPlace === 1) {
-  //   onesPlace = "I";
-  // } else if (onesPlace === 2) {
-  //   onesPlace = "II";
-  // } else if (onesPlace === 3) {
-  //   onesPlace = "III";
-  // } else if (onesPlace === 4) {
-  //   onesPlace = "IV";
-  // }
-  // return onesPlace;
-
-
+  return thousandsPlace;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 describe("romanNumerals", function() {
 
-  it("will take a given number and split it into an array of digits", function(){
-    expect(romanNumerals(2362)).to.eql([2,3,6,2]);
-  });
+  // it("will take a given number and split it into an array of digits", function(){
+  //   expect(romanNumerals(2362)).to.eql([2,3,6,2]);
+  // });
 
   it("will throw error if number is longer than 4 digits", function(){
     expect(romanNumerals(12345)).to.equal(true);
@@ -11,6 +11,10 @@ describe("romanNumerals", function() {
   it("will throw error if digit in 1000's place is greater than 3", function() {
     expect(romanNumerals(4321)).to.equal(true);
   });
+
+  it("will put together ones, tens, hundreds, and thousands outputs", function() {
+    expect(romanNumerals(3000)).to.equal("MMM");
+  });
 });
 
 describe("ones", function() {
@@ -18,17 +22,36 @@ describe("ones", function() {
   it("check if index references correct position in numbers", function() {
     expect(ones(3)).to.equal("III");
   });
-
-  // it("will convert numbers 1-3 in 1's place to roman numerals", function(){
-  //   expect(ones(3)).to.equal("III");
-  // });
-  //
-  // it("will convert 4 in 1's place to roman numerals", function() {
-  //   expect(ones(4)).to.equal("IV");
-  // });
-  //
-  // it("will convert 5 in 1's place to roman numerals", function() {
-  //   expect(ones(5)).to.equal("V");
-  // });
-
 });
+
+describe("tens", function() {
+
+  it("check if index references correct position in numbers", function() {
+    expect(tens(3)).to.equal("XXX");
+  });
+});
+
+describe("hundreds", function() {
+
+  it("check if index references correct position in numbers", function() {
+    expect(hundreds(3)).to.equal("CCC");
+  });
+});
+
+describe("thousands", function() {
+
+  it("check if index references correct position in numbers", function() {
+    expect(thousands(3)).to.equal("MMM");
+  });
+});
+
+// function romanize (num) {
+//   if (!+num)
+//   return false;
+//   var digits = String(+num).split(""),
+//   key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+//          "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+//          "","I","II","III","IV","V","VI","VII","VIII","IX"],
+//   roman = "",
+//   i = 3;
+//   while (i--) roman = (key[+digits.pop() + (i * 10)] || "") + roman; return Array(+digits.join("") + 1).join("M") + roman; }
